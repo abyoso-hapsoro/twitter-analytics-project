daacac59fc14a872acf0f87f164d68a01ea8d9b7
Abyoso <abyoso.hapsoro@sci.ui.ac.id>
2021-11-27 18:39:10 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,11 @@
 import backend
 from flask import Flask, redirect, url_for, render_template, request
+from threading import Timer
+
+def send_blank_request(interval):
+  Timer(interval, send_blank_request, [interval]).start()
+  print('')
+send_blank_request(28)
 
 application = Flask(__name__, static_url_path = '/static', static_folder = 'static')
 
