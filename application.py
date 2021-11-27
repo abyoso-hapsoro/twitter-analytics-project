3d7bf4609812beca800b8258e7981e9eee1a0ab9
Abyoso <abyoso.hapsoro@sci.ui.ac.id>
2021-11-27 19:05:59 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,15 @@
 import backend
 from flask import Flask, redirect, url_for, render_template, request
+from flask_sockets import Sockets
 
 application = Flask(__name__, static_url_path = '/static', static_folder = 'static')
+sockets     = Sockets(application)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+  while not ws.closed:
+    message = ws.receive()
+    ws.send(message)
 
 @application.route('/')
 def hello():
@@ -13,4 +21,7 @@ def main():
   return render_template('out.html')
 
 if __name__ == '__main__':
-  application.run()
+  from gevent import pywsgi
+  from geventwebsocket.handler import WebSocketHandler
+  server = pywsgi.WSGIServer(('', 5000), application, handler_class = WebSocketHandler)
+  server.serve_forever()
