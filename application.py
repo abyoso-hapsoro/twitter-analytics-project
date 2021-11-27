8b8d8a1deeea4502ecc3260ddc172747921341a9
Abyoso <abyoso.hapsoro@sci.ui.ac.id>
2021-11-27 17:29:25 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
 import backend
 from flask import Flask, redirect, url_for, render_template, request
 
-app = Flask(__name__, static_url_path = '/static', static_folder = 'static')
+application = Flask(__name__, static_url_path = '/static', static_folder = 'static')
 
-@app.route('/')
+@application.route('/')
 def hello():
   return render_template('search.html')
 
-@app.route('/result', methods = ['POST'])
+@application.route('/result', methods = ['POST'])
 def main():
   backend.main(request)
   return render_template('out.html')
 
-app.run()
+if __name__ == '__main__':
+  application.run()
